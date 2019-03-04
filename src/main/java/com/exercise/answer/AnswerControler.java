9166a393eeed3cbc99b89a4e35b5276ce22d844b
Lucas Dornelles <lucasdornellesr@gmail.com>
2019-03-04 20:21:32 -0300

    explicited spring imports

--- src/main/java/com/exercise/answer/AnswerControler.java
+++ src/main/java/com/exercise/answer/AnswerControler.java
@@ -1,7 +1,9 @@
 package com.exercise.answer;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import static com.exercise.answer.util.RandomAnswer.parseAnswer;
